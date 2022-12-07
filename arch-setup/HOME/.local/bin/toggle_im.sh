732d1b5a77671955a7509a35d7f04c16d4251093
yuiiio <atbjyk@protonmail.com>
2022-12-07 21:17:45 +0900

    12/7

---
+++ arch-setup/HOME/.local/bin/toggle_im.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+FILE="$HOME/.config/running_fcitx5"
+
+if [ -e $FILE ]; then
+    #if $(pkill -9 fcitx5); then
+    pkill -9 fcitx5;
+    pkill -9 mozc;
+    rm $FILE;
+    #fi
+else
+    touch $FILE
+    fcitx5
+fi
