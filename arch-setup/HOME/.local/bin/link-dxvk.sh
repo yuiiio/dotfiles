935ec915305ff0bdc71ddc6194ea8ddf110cf0d3
yuiiio <atbjyk@protonmail.com>
2023-01-20 03:04:02 +0900

    1/20

--- arch-setup/HOME/.local/bin/link-dxvk.sh
+++ arch-setup/HOME/.local/bin/link-dxvk.sh
@@ -1,1 +1,4 @@
 ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.wine-lol/drive_c/windows/system32/
+
+ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk
+ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk
