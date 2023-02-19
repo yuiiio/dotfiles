dbab1d3fbd46bd2fd88b1c45b786f7cc39000b95
yuiiio <atbjyk@protonmail.com>
2023-02-19 14:04:21 +0900

    2/19

--- arch-setup/HOME/.local/bin/leagueoflegends.sh
+++ arch-setup/HOME/.local/bin/leagueoflegends.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEFSYNC=1 WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=b;dxgi=n" /opt/wine-lol/bin/wine $HOME/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
+WINE_CPU_TOPOLOGY=4:0,1,2,3 DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEFSYNC=1 WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=b;dxgi=n" /opt/wine-lol/bin/wine $HOME/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
