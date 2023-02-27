7fc26aaf94419b72133c06071a944ed0a0bf07cb
yuiiio <atbjyk@protonmail.com>
2023-02-28 08:19:19 +0900

    2/28

--- arch-setup/HOME/.local/bin/suspend_when_ac_offline.sh
+++ arch-setup/HOME/.local/bin/suspend_when_ac_offline.sh
@@ -4,8 +4,9 @@ if cat /sys/class/power_supply/AC0/online | grep 0 > /dev/null 2>&1
 then
     if pgrep steam >/dev/null || pgrep wine >/dev/null; then
         echo "steam or wine running"
-    else
-        systemctl suspend
+    #else
+        #maybe after add libliftoff interface, after resume, stack at black screen(UHD630, kernel bug?)
+        #systemctl suspend
     fi
 else
     echo "ac online so does not suspend"
