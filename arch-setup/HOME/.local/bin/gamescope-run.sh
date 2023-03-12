b221fbb1a26ff3727557e16539c38cf94f59ea2d
yuiiio <atbjyk@protonmail.com>
2023-03-13 00:52:17 +0900

    3/13

--- arch-setup/HOME/.local/bin/gamescope-run.sh
+++ arch-setup/HOME/.local/bin/gamescope-run.sh
@@ -1,1 +1,3 @@
-INTEL_DEBUG=noccs gamescope --prefer-vk-device 8086:3e9b -H 1080 -i -f -r 60 -o 60 -- $1
+IGPU_CARD=`readlink -f /dev/dri/by-path/pci-0000:00:02.0-card`
+
+WLR_DRM_DEVICES="$IGPU_CARD" INTEL_DEBUG=noccs $HOME/gamescope/build/gamescope --prefer-vk-device 8086:3e9b -H 1080 -i -f -r 60 -o 60 -- $1
