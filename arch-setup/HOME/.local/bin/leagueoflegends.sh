76b032900b90967344b44fba49f0bcab148f1cb1
yuiiio <atbjyk@protonmail.com>
2023-04-07 19:08:17 +0900

    4/7

--- arch-setup/HOME/.local/bin/leagueoflegends.sh
+++ arch-setup/HOME/.local/bin/leagueoflegends.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-WINE_CPU_TOPOLOGY=4:0,1,2,3 DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEFSYNC=1 WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=b;dxgi=n" /opt/wine-lol/bin/wine $HOME/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
+DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=b;dxgi=n" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
--- arch-setup/HOME/.local/bin/link-dxvk.sh
+++ arch-setup/HOME/.local/bin/link-dxvk.sh
@@ -1,4 +1,5 @@
-ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.wine-lol/drive_c/windows/system32/
+ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.wine-lol/drive_c/windows/system32/
+ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.wine-lol/drive_c/windows/syswow64/
 
 ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk
 ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk
