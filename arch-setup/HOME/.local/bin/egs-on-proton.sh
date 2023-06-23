74e4fe043e4e90a2638c674eeecbf4501809d83e
yuiiio <atbjyk@protonmail.com>
2023-06-23 14:49:42 +0900

    6/23

--- arch-setup/HOME/.local/bin/egs-on-proton.sh
+++ arch-setup/HOME/.local/bin/egs-on-proton.sh
@@ -1,1 +1,3 @@
 VKD3D_CONFIG=dxr,dxr11,no_upload_hvv VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay DXVK_FILTER_DEVICE_NAME="GTX 1050" STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh /home/haibane/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
+
+# -opengl -SkipBuildPatchPrereq
