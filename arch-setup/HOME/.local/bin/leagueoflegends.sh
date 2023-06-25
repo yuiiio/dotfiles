1237fbcb4b7bfa3da80c9ac3b562c15e0ffea3de
yuiiio <atbjyk@protonmail.com>
2023-06-25 20:08:47 +0900

    leagueoflegends.sh start from riotclientservice

--- arch-setup/HOME/.local/bin/leagueoflegends.sh
+++ arch-setup/HOME/.local/bin/leagueoflegends.sh
@@ -2,4 +2,6 @@
 
 # DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=b;dxgi=n" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
 
-DXVK_FILTER_DEVICE_NAME="UHD Graphics 630" WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
+# WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
+
+WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/Riot\ Client/RiotClientServices.exe
