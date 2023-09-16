5bf25d15e20749d983f0456b8263b5bd5675637a
yuiiio <atbjyk@protonmail.com>
2023-09-17 02:05:26 +0900

    9/17

--- arch-setup/HOME/.local/bin/show_volume.sh
+++ arch-setup/HOME/.local/bin/show_volume.sh
@@ -1,3 +1,6 @@
-SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
-NOW=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
-echo $NOW
+# SINK=$( pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+# NOW=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+# echo $NOW
+
+STR=$(amixer get Master | grep -E -o -m 1 '[0-9]{1,3}%')
+echo ${STR%?}
---
+++ arch-setup/HOME/.local/bin/volume_down.sh
@@ -0,0 +1,5 @@
+pactl set-sink-volume @DEFAULT_SINK@ -5%
+
+# for plasma osd
+NOW=$(show_volume.sh)
+qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged ${NOW}
