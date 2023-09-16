c4e2274881b7035fc635235bc7a976f0a51131a8
yuiiio <atbjyk@protonmail.com>
2023-09-16 22:12:21 +0900

    9/16

--- arch-setup/HOME/.local/bin/leagueoflegends.sh
+++ arch-setup/HOME/.local/bin/leagueoflegends.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b;RiotClientCrashHandler.exe=d;LeagueCrashHandler64.exe=d" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/Riot\ Client/RiotClientServices.exe
-
-# WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b;" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
-
-WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=b;d3d9=b;dxgi=b;RiotClientCrashHandler.exe=d;" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
+WINEDEBUG=-all WINEARCH=win64 WINEPREFIX=$HOME/.wine-lol WINEDLLOVERRIDES="winemenubuilder.exe=d;d3d11=n;d3d9=n;dxgi=n;RiotClientCrashHandler.exe=d;" /usr/bin/wine /home/haibane/.wine-lol/drive_c/Riot\ Games/League\ of\ Legends/LeagueClient.exe
