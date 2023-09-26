f5ce59cfcfa4c2c8ba9a63429c30aa8e3ae26291
yuiiio <atbjyk@protonmail.com>
2023-09-27 01:15:12 +0900

    9/27.3

--- arch-setup/HOME/.local/bin/toggle_bemenu.sh
+++ arch-setup/HOME/.local/bin/toggle_bemenu.sh
@@ -9,6 +9,7 @@ if [ -e $FILE ]; then
     #fi
 else
     touch $FILE
-    bemenu-run --tb "#285577" --tf "#FFFFFF" --hb "#4C7899" --hf "#FFFFFF" --fb "#333333"  --nb "#333333" --fn 'monospace 14'
+    # bemenu-run --tb "#285577" --tf "#FFFFFF" --hb "#4C7899" --hf "#FFFFFF" --fb "#333333"  --nb "#333333" --fn 'monospace 14'
+    bemenu-run --tb "#88c0d0" --tf "#2e3440" --hb "#88c0d0" --hf "#2e3440" --fb "#2E3440" --nb "#2E3440" --ab "#d8dee9" --af "#2e3440" --fn 'monospace 13'
     rm $FILE
 fi
