ef7cca02ddb15dc0d3f95a3d8205896a6bb8c859
yuiiio <atbjyk@protonmail.com>
2023-09-26 19:33:14 +0900

    9/26

--- arch-setup/HOME/.local/bin/suspend_when_ac_offline.sh
+++ arch-setup/HOME/.local/bin/suspend_when_ac_offline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if cat /sys/class/power_supply/AC0/online | grep 0 > /dev/null 2>&1
+if cat /sys/class/power_supply/AC/online | grep 0 > /dev/null 2>&1
 then
     if pgrep steam >/dev/null || pgrep wine >/dev/null; then
         echo "steam or wine running"
