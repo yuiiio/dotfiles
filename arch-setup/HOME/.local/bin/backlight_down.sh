a401af73c09ce744a07d7e9bf943e4d0eb59d848
yuiiio <atbjyk@protonmail.com>
2023-12-21 19:55:44 +0900

    for desktop

--- arch-setup/HOME/.local/bin/backlight_down.sh
+++ arch-setup/HOME/.local/bin/backlight_down.sh
@@ -3,5 +3,6 @@
 if pgrep steam >/dev/null || pgrep wine >/dev/null; then
     echo "steam or wine running"
 else
-    swaymsg "output * dpms off"
+    #swaymsg "output * dpms off"
+    wayout --off DP-2
 fi
