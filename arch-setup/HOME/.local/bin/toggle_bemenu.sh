a65d5c742b3fd4c86888d7029237385456dd1401
yuiiio <atbjyk@protonmail.com>
2024-02-09 00:48:15 +0900

    bemenu: mergin 20

--- arch-setup/HOME/.local/bin/toggle_bemenu.sh
+++ arch-setup/HOME/.local/bin/toggle_bemenu.sh
@@ -10,6 +10,6 @@ if [ -e $FILE ]; then
 else
     touch $FILE
     # bemenu-run --tb "#285577" --tf "#FFFFFF" --hb "#4C7899" --hf "#FFFFFF" --fb "#333333"  --nb "#333333" --fn 'monospace 14'
-    bemenu-run --tb "#88c0d0" --tf "#2e3440" --ab "#88c0d0" --af "#2e3440" --fb "#2E3440" --nb "#2E3440" --hb "#d8dee9" --hf "#2e3440" --fn 'monospace 13'
+    bemenu-run --tb "#88c0d0" --tf "#2e3440" --ab "#88c0d0" --af "#2e3440" --fb "#2E3440" --nb "#2E3440" --hb "#d8dee9" --hf "#2e3440" --fn 'monospace 13' -M 20
     rm $FILE
 fi
