74de04016e8aea2f55760588c279bba039313324
yuiiio <atbjyk@protonmail.com>
2024-03-18 16:24:41 +0900

    add dlfcd.service

--- arch-setup/HOME/.local/bin/rpcs3.sh
+++ arch-setup/HOME/.local/bin/rpcs3.sh
@@ -1,2 +1,2 @@
 # qt fcitx5 dbus module cuase segmentation fault on startup
-QT_QPA_PLATFORM=xcb VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay $HOME/rpcs3_build/bin/rpcs3
+QT_QPA_PLATFORM=xcb _VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay $HOME/rpcs3_build/bin/rpcs3
