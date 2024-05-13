405fc83511911069b4a26e014c09f552a5eab300
yuiiio <atbjyk@protonmail.com>
2024-05-14 00:11:31 +0900

    link dxvk vkd3d also compat folder custom build proton

--- arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
+++ arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
@@ -6,6 +6,12 @@ ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.wine-lol/drive_c/windows/syswow64/
 ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk/
 ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/
 
+ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib64/wine/dxvk/
+ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/dxvk/
+
 # vkd3d for proton exp
 ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/vkd3d-proton/
 ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/
+
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib64/wine/vkd3d-proton/
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/vkd3d-proton/
