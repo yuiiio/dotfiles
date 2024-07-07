336830b09e8c28ab2aa0f762b02057541420c515
yuiiio <atbjyk@protonmail.com>
2024-07-07 12:03:38 +0900

    backlight-on-off-tool when suspend using wlopm

--- arch-setup/HOME/.local/bin/backlight_down.sh
+++ arch-setup/HOME/.local/bin/backlight_down.sh
@@ -3,6 +3,6 @@
 if pgrep steam >/dev/null || pgrep wine || pgrep rpcs3 >/dev/null; then
     echo "steam or wine running"
 else
-    wayout --off eDP-1
+    wlopm --off \*
     #swaymsg "output * dpms off"
 fi
