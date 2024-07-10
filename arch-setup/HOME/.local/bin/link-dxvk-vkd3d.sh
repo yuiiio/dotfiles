ce74253b0b294edaafe13df0bccd1a83497ab4c9
yuiiio <atbjyk@protonmail.com>
2024-07-10 20:15:41 +0900

    wine-lol is dead

--- arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
+++ arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
@@ -1,7 +1,3 @@
-# dxvk for wine-lol
-ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.wine-lol/drive_c/windows/system32/
-ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.wine-lol/drive_c/windows/syswow64/
-
 # dxvk for proton exp
 ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk/
 ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/
