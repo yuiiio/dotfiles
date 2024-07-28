00a4d4e18d2d80a41b6241b428f95de4fb0c9f7b
yuiiio <atbjyk@protonmail.com>
2024-07-28 16:56:31 +0900

    set gpu minimum clock to 1200Mhz if performance profile with power-profiles-daemon

--- arch-setup/etc/power-profiles.d/performance/02-gpu-power-dpm-performance.sh
+++ arch-setup/etc/power-profiles.d/performance/02-gpu-power-dpm-performance.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+/usr/bin/tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< manual
 /usr/bin/tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< auto
+# seems need toggle twice after sleep
+
+/usr/bin/tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< manual
+
+/usr/bin/tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "s 0 1200"
+/usr/bin/tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "c"
+
+# set gpu minimum clock to 1200Mhz.(for gaming)
+# if using apu, cpu power waste and will be limits at like ~2.5GHz if gpu usage is high.
+# but if gpu usage is low, cpu can clock at High(~4GHz)
+# so expects no other negative effect.
