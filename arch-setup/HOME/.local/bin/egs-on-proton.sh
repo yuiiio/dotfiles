ac27a1ad5737603c4e132efe4184c5f63f0a7759
yuiiio <atbjyk@protonmail.com>
2024-08-04 17:14:58 +0900

    update dxvk.conf

--- arch-setup/HOME/.local/bin/egs-on-proton.sh
+++ arch-setup/HOME/.local/bin/egs-on-proton.sh
@@ -1,6 +1,7 @@
 cd /home/haibane/.proton-egs
 
-VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh /home/haibane/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
+# unset DXVK_HUD, for use dxvk.conf [APP.exe]
+DXVK_HUD= VK_INSTANCE_LAYERS=_VK_LAYER_MESA_overlay STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh /home/haibane/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
 
 
 # -opengl -SkipBuildPatchPrereq
