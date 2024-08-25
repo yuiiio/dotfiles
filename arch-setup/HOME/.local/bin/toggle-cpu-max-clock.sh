e5d6e89215ed2825308be94f334b313fd0152324
yuiiio <atbjyk@protonmail.com>
2024-08-25 09:33:20 +0900

    clock toggle script update

--- arch-setup/HOME/.local/bin/toggle-cpu-max-clock.sh
+++ arch-setup/HOME/.local/bin/toggle-cpu-max-clock.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
 FILE="/tmp/cpu_max_clock_set"
 
+# 1: state+, 2: state-
+CYCLE_DIR=$1
+
 if [ -e $FILE ]; then
-    #sudo cpupower frequency-set --max 4.4Ghz
-    sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 4400000
-    rm $FILE
+    # 1: 0.8GHz, 2: 1.8GHz, 3: 4.4GHz
+    VAR=$(cat $FILE)
+    if [ $CYCLE_DIR = "1" ]; then
+        if [ $VAR = "1" ]; then
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 1800000
+            echo "2" > $FILE
+        elif [ $VAR = "2" ]; then
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 4400000
+            echo "3" > $FILE
+        elif [ $VAR = "3" ]; then # cycle
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 800000
+            echo "1" > $FILE
+        fi
+    elif [ $CYCLE_DIR = "2" ]; then # reverse
+        if [ $VAR = "1" ]; then # cycle
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 4400000
+            echo "3" > $FILE
+        elif [ $VAR = "2" ]; then
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 800000
+            echo "1" > $FILE
+        elif [ $VAR = "3" ]; then
+            sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 1800000
+            echo "2" > $FILE
+        fi
+    fi
 else
-    touch $FILE
-    #sudo cpupower frequency-set --max 1.8Ghz
+    # if not file exist(first run), set at 2: 1.8GHz and create tmp file.
     sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 1800000
+    touch $FILE
+    echo "2" > $FILE
 fi
--- arch-setup/HOME/.local/bin/toggle-gpu-clock.sh
+++ arch-setup/HOME/.local/bin/toggle-gpu-clock.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
 FILE="/tmp/manual_gpu_clock"
 
-if [ -e $FILE ]; then
-    sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
-    # echo "exit gamemode"
-    rm $FILE
-else
-    touch $FILE
-    sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
-    sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
-    # seems need toggle twice after sleep
+# 1: state+, 2: state-
+CYCLE_DIR=$1
 
-    sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
-    sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "s 0 1200"
-    sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "c"
-    # echo "gamemode gpu clock"
+if [ -e $FILE ]; then
+    # 1: low, 2: auto, 3: manual(1200MHz)
+    VAR=$(cat $FILE)
+    if [ $CYCLE_DIR = "1" ]; then
+        if [ $VAR = "1" ]; then
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
+            echo "2" > $FILE
+        elif [ $VAR = "2" ]; then
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
+            # seems need toggle twice after sleep
+
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
+            sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "s 0 1200"
+            sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "c"
+            echo "3" > $FILE
+        elif [ $VAR = "3" ]; then # cycle
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "low"
+            echo "1" > $FILE
+        fi
+    elif [ $CYCLE_DIR = "2" ]; then # reverse
+        if [ $VAR = "1" ]; then # cycle
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
+            # seems need toggle twice after sleep
+
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "manual"
+            sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "s 0 1200"
+            sudo tee /sys/class/drm/card0/device/pp_od_clk_voltage <<< "c"
+            echo "3" > $FILE
+        elif [ $VAR = "2" ]; then
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "low"
+            echo "1" > $FILE
+        elif [ $VAR = "3" ]; then
+            sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
+            echo "2" > $FILE
+        fi
+    fi
+else
+    # if not exist(first run), set at auto and create tmp file 
+    sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level <<< "auto"
+    touch $FILE
+    echo "2" > $FILE
 fi
