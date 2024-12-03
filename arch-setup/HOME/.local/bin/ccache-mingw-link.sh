12c0f7a8ee5d49fad8b22ef42c9180df6328e49b
yuiiio <atbjyk@protonmail.com>
2024-12-04 06:18:30 +0900

    fix ccache-mingw-link.sh

--- arch-setup/HOME/.local/bin/ccache-mingw-link.sh
+++ arch-setup/HOME/.local/bin/ccache-mingw-link.sh
@@ -1,6 +1,6 @@
 # for mingw & ccache
 
-export MINGW_GCC_VERSION=`x86_64-w64-mingw32-gcc --version | grep gcc | awk '{print $3}'` 
+export MINGW_GCC_VERSION=`/usr/bin/x86_64-w64-mingw32-gcc --version | grep gcc | awk '{print $3}'`
 
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/x86_64-w64-mingw32-c++
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/x86_64-w64-mingw32-cc
@@ -14,5 +14,4 @@ sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-cc
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-cpp
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-g++
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-gcc
-sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-gcc-$mingw_gcc_version
-
+sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/i686-w64-mingw32-gcc-$MINGW_GCC_VERSION
