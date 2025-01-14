bc64be75322575f28e891c3705255cb0a10162a1
yuiiio <atbjyk@protonmail.com>
2025-01-15 00:10:06 +0900

    update cmake rpcs3 option (znver2)

--- arch-setup/HOME/.local/bin/cmake-rpcs3-build.sh
+++ arch-setup/HOME/.local/bin/cmake-rpcs3-build.sh
@@ -4,16 +4,21 @@ export LINKER=lld
 export AR=/usr/bin/llvm-ar
 export RANLIB=/usr/bin/llvm-ranlib
 
-export CFLAGS="$CFLAGS -fuse-ld=${LINKER}"
+#export CFLAGS="-march=znver2 -mtune=native -flto=thin -fuse-ld=gold -O2 -pipe -fPIC"
+export CFLAGS="-march=znver2 -mtune=native -fuse-ld=gold -O2 -pipe -fPIC"
+export CFLAGS="${CFLAGS} -fuse-ld=${LINKER}"
+
+CXXFLAGS="${CFLAGS}"
+LDFLAGS="-Wl,-O1,--as-needed,--sort-common,-z,relro ${CFLAGS}"
 
 cmake ../rpcs3 \
     -DUSE_SDL=ON \
     -DUSE_SYSTEM_SDL=ON \
     -DUSE_SYSTEM_FFMPEG=ON \
     -DUSE_FAUDIO=ON \
-    -DCMAKE_C_FLAGS="$CFLAGS" \
-    -DCMAKE_CXX_FLAGS="$CFLAGS" \
-    -DCMAKE_AR="$AR" \
-    -DCMAKE_RANLIB="$RANLIB" \
+    -DCMAKE_C_FLAGS="${CFLAGS}" \
+    -DCMAKE_CXX_FLAGS="${CFLAGS}" \
+    -DCMAKE_AR="${AR}" \
+    -DCMAKE_RANLIB="${RANLIB}" \
 
 make -j16
