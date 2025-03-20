0f1c18f2fc2120ac631d5d88c4288f0571a2bdad
yuiiio <atbjyk@protonmail.com>
2025-03-21 08:05:30 +0900

    3/21

--- arch-setup/HOME/.local/bin/egs-on-proton.sh
+++ arch-setup/HOME/.local/bin/egs-on-proton.sh
@@ -2,7 +2,7 @@ cd /home/haibane/.proton-egs
 
 #VK_INSTANCE_LAYERS=_VK_LAYER_MESA_overlay STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh /home/haibane/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
 
-DXVK_HUD=0 VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/compatibilitytools.d/proton_tkg_experimental.bleeding.edge.9.0.173260.20250312/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
+DXVK_HUD=0 VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-egs STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/proton run /home/haibane/.proton-egs/pfx/drive_c/Program\ Files\ \(x86\)/Epic\ Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe
 
 
 # -opengl -SkipBuildPatchPrereq
