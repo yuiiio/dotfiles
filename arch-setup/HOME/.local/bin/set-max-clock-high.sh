b8c0368c70bcaa4d8c29de14d117f6fcdaab0f89
yuiiio <atbjyk@protonmail.com>
2025-04-18 22:29:19 +0900

    cpu clock script

---
+++ arch-setup/HOME/.local/bin/set-max-clock-high.sh
@@ -0,0 +1,1 @@
+sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq <<< 5883197
---
+++ arch-setup/HOME/.local/bin/show-cur-max-clock.sh
@@ -0,0 +1,2 @@
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+# cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq
