fb6781ae01af46c8a88138dee057335410c561ca
yuiiio <atbjyk@protonmail.com>
2025-05-07 05:09:08 +0900

    update link-dxvk-vkd3d.sh for newer proton >=10

--- arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
+++ arch-setup/HOME/.local/bin/link-dxvk-vkd3d.sh
@@ -1,13 +1,15 @@
 # dxvk for proton exp
-ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/dxvk/
-ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/
+ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/x86_64-windows/
+ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/dxvk/i386-windows/
 
-ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib64/wine/dxvk/
-ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/dxvk/
 
 # vkd3d for proton exp
-ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/vkd3d-proton/
-ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/x86_64-windows
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/i386-windows/
 
-ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib64/wine/vkd3d-proton/
-ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/vkd3d-proton/
+# for local build
+ln -sf ~/dxvk-build/dxvk-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/dxvk/x86_64-windows/
+ln -sf ~/dxvk-build/dxvk-master/x32/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/dxvk/i386-windows/
+
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x64/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/vkd3d-proton/x86_64-windows/
+ln -sf ~/vkd3d-proton-build/vkd3d-proton-master/x86/* ~/.local/share/Steam/compatibilitytools.d/*/files/lib/wine/vkd3d-proton/i386-windows/
