9abd741bd4e08f3f08ee8dbca541d64c8502ace0
yuiiio <atbjyk@protonmail.com>
2025-09-11 18:57:53 +0900

    update ccache migw link script

--- arch-setup/HOME/.local/bin/ccache-mingw-link.sh
+++ arch-setup/HOME/.local/bin/ccache-mingw-link.sh
@@ -1,5 +1,9 @@
 # for mingw & ccache
 
+# remove old link
+sudo rm /usr/lib/ccache/bin/x86_64-w64-mingw32-gcc-*
+sudo rm /usr/lib/ccache/bin/i686-w64-mingw32-gcc-*
+
 export MINGW_GCC_VERSION=`/usr/bin/x86_64-w64-mingw32-gcc --version | grep gcc | awk '{print $3}'`
 
 sudo ln -sf /usr/bin/ccache  /usr/lib/ccache/bin/x86_64-w64-mingw32-c++
