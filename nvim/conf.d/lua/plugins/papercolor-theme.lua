5fea342dbe525692559f986be65a0112ede5c6f6
yuiiio <atbjyk@protonmail.com>
2025-10-19 19:29:50 +0900

    packar => lazy.nvim

---
+++ nvim/conf.d/lua/plugins/papercolor-theme.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "NLKNguyen/papercolor-theme",
+        config = function()
+            vim.cmd("colorscheme PaperColor")
+            vim.cmd("set background=light")
+        end,
+    }
+}
