3a8318877f1fd1316ddea4f4bd31ced37562e704
yuiiio <atbjyk@protonmail.com>
2025-11-02 00:57:36 +0900

    can use wayland backend when compositor support fifo-v1

--- arch-setup/HOME/.local/bin/rpcs3.sh
+++ arch-setup/HOME/.local/bin/rpcs3.sh
@@ -1,2 +1,2 @@
 # qt fcitx5 dbus module cuase segmentation fault on startup
-_RADV_PERFTEST=nogttspill QT_QPA_PLATFORM=xcb _VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay $HOME/build_rpcs3/bin/rpcs3
+_RADV_PERFTEST=nogttspill QT_QPA_PLATFORM=wayland _VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay $HOME/build_rpcs3/bin/rpcs3
