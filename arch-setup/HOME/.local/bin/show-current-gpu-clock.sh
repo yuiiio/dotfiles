b6faaea8a3376d75e03932205947e22efc95e872
yuiiio <atbjyk@protonmail.com>
2025-12-01 03:13:14 +0900

    waybar: style

--- arch-setup/HOME/.local/bin/show-current-gpu-clock.sh
+++ arch-setup/HOME/.local/bin/show-current-gpu-clock.sh
@@ -1,8 +1,8 @@
 CLOCK_STATE=$(cat /sys/class/drm/card0/device/power_dpm_force_performance_level)
 if [ "$CLOCK_STATE" = "manual" ]; then
-    echo "mnal"
+    echo "Mnal"
 elif [ "$CLOCK_STATE" = "auto" ]; then
-    echo "auto"
+    echo "Auto"
 elif [ "$CLOCK_STATE" = "low" ]; then
-    echo "low"
+    echo "Low"
 fi
