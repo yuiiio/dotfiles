016318eb9413ff4d020a29da85cd762e091bcb68
yuiiio <atbjyk@protonmail.com>
2026-02-22 16:00:41 +0900

    Revert "light theme"
    This reverts commit 3cb56c4dd82f7594319dc465cf2b700206cbb8de.

--- nvim/conf.d/lua/plugins/papercolor-theme.lua
+++ nvim/conf.d/lua/plugins/papercolor-theme.lua
@@ -2,8 +2,9 @@ return {
     {
         "NLKNguyen/papercolor-theme",
         config = function()
-            vim.cmd("colorscheme PaperColor")
-            vim.cmd("set background=light")
+            -- vim.cmd("colorscheme PaperColor")
+            -- vim.cmd("set background=light")
+            vim.cmd("colorscheme habamax")
         end,
     }
 }
