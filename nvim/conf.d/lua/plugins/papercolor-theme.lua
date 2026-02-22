3cb56c4dd82f7594319dc465cf2b700206cbb8de
yuiiio <atbjyk@protonmail.com>
2026-02-22 15:56:39 +0900

    light theme

--- nvim/conf.d/lua/plugins/papercolor-theme.lua
+++ nvim/conf.d/lua/plugins/papercolor-theme.lua
@@ -2,9 +2,8 @@ return {
     {
         "NLKNguyen/papercolor-theme",
         config = function()
-            -- vim.cmd("colorscheme PaperColor")
-            -- vim.cmd("set background=light")
-            vim.cmd("colorscheme habamax")
+            vim.cmd("colorscheme PaperColor")
+            vim.cmd("set background=light")
         end,
     }
 }
