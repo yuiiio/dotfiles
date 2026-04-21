32143fc68a809054c36b42869b2a80ef33e58b4b
yuiiio <atbjyk@protonmail.com>
2026-04-21 16:13:25 +0900

    4/21

--- arch-setup/HOME/.local/bin/atelier-ayesha.sh
+++ arch-setup/HOME/.local/bin/atelier-ayesha.sh
@@ -1,6 +1,6 @@
 
-cd $HOME/.proton-local/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Ayesha\ The\ Alchemist\ of\ Dusk\ DX/
+cd $HOME/.proton-unlock/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Ayesha\ The\ Alchemist\ of\ Dusk\ DX/
 # VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay
 # WINE_CPU_TOPOLOGY=4:0,1,2,3
 
-LANG=ja_JP.UTF-8 PROTON_DISABLE_HIDRAW=1 LD_LIBRARY_PATH=$HOME/.local/share/Steam/ubuntu12_32:$HOME/.local/share/Steam/ubuntu12_64:$LD_LIBRARY_PATH STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-local STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/compatibilitytools.d/GE-Proton10-26/proton run ~/.proton-local/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Ayesha\ The\ Alchemist\ of\ Dusk\ DX/Atelier_AyeshaLauncher.exe 
+LANG=ja_JP.UTF-8 PROTON_DISABLE_HIDRAW=1 LD_LIBRARY_PATH=$HOME/.local/share/Steam/ubuntu12_32:$HOME/.local/share/Steam/ubuntu12_64:$LD_LIBRARY_PATH STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-unlock STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/compatibilitytools.d/GE-Proton10-26/proton run ~/.proton-unlock/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Ayesha\ The\ Alchemist\ of\ Dusk\ DX/Atelier_AyeshaLauncher.exe 
---
+++ arch-setup/HOME/.local/bin/atelier-escha-logy.sh
@@ -0,0 +1,6 @@
+
+cd $HOME/.proton-unlock/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Escha\ and\ Logy\ Alchemists\ of\ the\ Dusk\ Sky\ DX/
+# VK_INSTANCE_LAYERS=VK_LAYER_MESA_overlay
+# WINE_CPU_TOPOLOGY=4:0,1,2,3
+
+LANG=ja_JP.UTF-8 PROTON_DISABLE_HIDRAW=1 LD_LIBRARY_PATH=$HOME/.local/share/Steam/ubuntu12_32:$HOME/.local/share/Steam/ubuntu12_64:$LD_LIBRARY_PATH STEAM_COMPAT_DATA_PATH=/home/haibane/.proton-unlock STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/haibane/.local/share/Steam /home/haibane/.local/share/Steam/compatibilitytools.d/GE-Proton10-26/proton run ~/.proton-unlock/pfx/drive_c/Program\ Files\ \(x86\)/Atelier\ Escha\ and\ Logy\ Alchemists\ of\ the\ Dusk\ Sky\ DX/Atelier_Escha_and_LogyLauncher.exe
