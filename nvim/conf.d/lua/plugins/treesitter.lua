2924b3c7dc8361c9a090dcee58b1f6bf76119dcf
yuiiio <atbjyk@protonmail.com>
2026-07-08 16:20:36 +0900

    treesitter master=>main Lazy update

--- nvim/conf.d/lua/plugins/treesitter.lua
+++ nvim/conf.d/lua/plugins/treesitter.lua
@@ -1,3 +1,3 @@
 return {
-    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+    {"nvim-treesitter/nvim-treesitter", branch = 'main', lazy = false, build = ":TSUpdate"}
 }
