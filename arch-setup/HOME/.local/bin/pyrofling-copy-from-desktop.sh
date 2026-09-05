f50b0474b6a6d6597d8ec55bb5f72b12003cad3c
yuiiio <atbjyk@protonmail.com>
2026-09-05 22:47:58 +0900

    pyrofling copy script

---
+++ arch-setup/HOME/.local/bin/pyrofling-copy-from-desktop.sh
@@ -0,0 +1,11 @@
+rsync -a desktop:$HOME/.local/bin/pyrofling                                                                    $HOME/.local/bin/pyrofling
+rsync -a desktop:$HOME/.local/bin/pyrofling-viewer                                                             $HOME/.local/bin/pyrofling-viewer
+rsync -a desktop:$HOME/.local/bin/pyrofling-latency-tester                                                     $HOME/.local/bin/pyrofling-latency-tester
+rsync -a desktop:$HOME/.local/bin/pyrofling-gamepad                                                            $HOME/.local/bin/pyrofling-gamepad
+rsync -a desktop:$HOME/.local/lib/libVkLayer_pyrofling_capture_64.so                                           $HOME/.local/lib/libVkLayer_pyrofling_capture_64.so
+rsync -a desktop:$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_capture_64.json                  $HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_capture_64.json
+rsync -a desktop:$HOME/.local/lib/libVkLayer_pyrofling_cross_wsi_64.so                                         $HOME/.local/lib/libVkLayer_pyrofling_cross_wsi_64.so
+rsync -a desktop:$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_cross_wsi_64.json                $HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_cross_wsi_64.json
+rsync -a desktop:$HOME/.local/lib/libVkLayer_pyrofling_latency_measurement_64.so                               $HOME/.local/lib/libVkLayer_pyrofling_latency_measurement_64.so
+rsync -a desktop:$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_latency_measurement_64.json      $HOME/.local/share/vulkan/implicit_layer.d/VkLayer_pyrofling_latency_measurement_64.json
+
